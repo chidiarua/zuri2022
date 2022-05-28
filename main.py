462efbaf2becdb046cda99ea208b31e4d2014c32
chidiarua <chidiarua99@gmail.com>
2022-05-28 09:04:00 +0100

    python anagram script

---
+++ main.py
@@ -0,0 +1,14 @@
+# Check if two words are anagrams
+# Example:
+# find_anagrams("hello", "check") --> False
+# find_anagrams("below", "elbow") --> True
+
+
+def find_anagram(word, anagram):
+    if sorted(word) != sorted(anagram):
+        return False
+    else:
+        return True
+
+
+print(find_anagram("below", "elbow"))
